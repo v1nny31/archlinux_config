06bc465f3548909612c0e4ed16157ba3f3e13a9e
Vincent <2485479@cstjean.qc.ca>
2025-09-04 11:50:08 -0400

    Exercice 2 fini

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+mkdir -p "$HOME/bin" "$HOME/cegep" "$HOME/projets"
+sudo pacman -S --noconfirm git vim gcc gdb python python-pip
+
+cat > "$HOME/.gitconfig"  << 'EOF'
+[user]
+    name = Vincent
+    email = 2485479@cstjean.qc.ca
+[core]
+    editor = code
+[github]
+    user = v1nny31
+EOF
+
 cp .bashrc ~/.bashrc
